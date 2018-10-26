98b988f596e3fb3e0aa3730b882e3cc9a87c081b
vipul1298 <b116063@iiit-bh.ac.in>
2018-10-26 22:16:22 +0530

    3rd assignment

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-var MongoClient = require('mongodb').MongoClient,
+var express = require('express'),
+    app = express(),
     crypto = require('crypto'),
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient;
     assert = require('assert');
 
-MongoClient.connect('mongodb://localhost:27017/m101', function(err, db) {
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
+MongoClient.connect("mongodb://localhost:27017/m101", function(err, db) {
+
     if(err) throw err;
 
-    var algorithm = 'aes256';
-    var encrypted_message = '7013254dca77e2c913d18cf5b70e7bba';
+    console.log("Successfully connected to MongoDB.");
 
-    db.collection('hw1_2').find({}).toArray(function(err, docs) {
-        if(err) throw err;
+    app.get('/', function(req, res) {
 
-        if (docs.length < 1) {
-            console.dir("No documents found");
-            return db.close();
-        }
-        
-	var doc = docs[0];
-        var decipher = crypto.createDecipher(algorithm, doc['_id']);
-        var decrypted = decipher.update(encrypted_message, 'hex', 'utf8') + decipher.final('utf8');
-        console.log("Answer: " + decrypted);
-        return db.close();
+        var algorithm = 'aes256';
+        var encrypted_message = "f36731a12e6130f0ed0bccbfd9bd6ebd";
+
+        db.collection('hw1_3').find({}).toArray(function(err, docs) {
+            if(err) throw err;
+
+            if (docs.length < 1) {
+                console.dir("No documents found. Did you forget to mongorestore?");
+                return res.send("No documents found. Did you forget to mongorestore?");
+            }
+
+	    var doc = docs[0];
+            var decipher = crypto.createDecipher(algorithm, doc['_id']);
+            var decrypted = decipher.update(encrypted_message, 'hex', 'utf8') + decipher.final('utf8');
+            return res.render('hello', { "name" : decrypted });
+        });
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log("Express server listening on port %s.", port);
     });
 });
